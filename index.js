2c271e853b9d9a5f22c564b054c355a735d9d12b
MAHTAB ALAM <100292587+rehan749@users.noreply.github.com>
2023-01-08 23:32:14 +0530

    complete

---
+++ index.js
@@ -0,0 +1,33 @@
+
+
+const toggleButton = document.getElementsByClassName('toggle-button')[0]
+const navbarLinks = document.getElementsByClassName('navbar-links')[0]
+
+toggleButton.addEventListener('click', () => {
+  navbarLinks.classList.toggle('active')
+})
+
+// const toggleButton = document.getElementsByClassName('toggle-button')[0]
+const navbarMenu = document.getElementsByClassName('navbar-menu')[0]
+
+toggleButton.addEventListener('click', () => {
+  navbarMenu.classList.toggle('active')
+})
+
+const loginText = document.querySelector(".title-text .login");
+      const loginForm = document.querySelector("form.login");
+      const loginBtn = document.querySelector("label.login");
+      const signupBtn = document.querySelector("label.signup");
+      const signupLink = document.querySelector("form .signup-link a");
+      signupBtn.onclick = (()=>{
+        loginForm.style.marginLeft = "-50%";
+        loginText.style.marginLeft = "-50%";
+      });
+      loginBtn.onclick = (()=>{
+        loginForm.style.marginLeft = "0%";
+        loginText.style.marginLeft = "0%";
+      });
+      signupLink.onclick = (()=>{
+        signupBtn.click();
+        return false;
+      });
